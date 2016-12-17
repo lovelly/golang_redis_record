b809e7da0c275d6a1c276179ccc69bdb4c4a31f7
zhangjiaowen <zhangjiaowen@changit.cn>
2016-12-17 14:34:52 +0800

    update

--- redis_record.go
+++ redis_record.go
@@ -1,8 +1,12 @@
-package gorecord
+package record
 
 import (
+	"fmt"
 	"reflect"
 	"strconv"
+
+	"changit.cn/contra/server/zaplogger"
+	"github.com/uber-go/zap"
 
 	redis "gopkg.in/redis.v4"
 )
@@ -13,160 +17,129 @@ type RdsRecord struct {
 	dbIndex int //在哪个数据库
 	keyname string
 	db      *redis.Client //
-	data    map[string]interface{}
-	flg     map[string]bool
+	flg     uint64
 }
 
 func NewResRecod(index int, name string, db *redis.Client) *RdsRecord {
 	v := &RdsRecord{dbIndex: index, keyname: name, db: db}
-	v.data = make(map[string]interface{})
-	v.flg = make(map[string]bool)
 	return v
 }
 
-func (r *RdsRecord) Setflog(key string) {
-	r.flg[key] = true
+func (r *RdsRecord) SetFlg(index uint64) {
+	index = 1 << index
+	r.flg = r.flg | index
 }
 
-//获取设置float64
-func (r *RdsRecord) getFloat64(name string) (float64, bool) {
-	v, ok := r.data[name]
-	if !ok {
-		return 0, false
-	}
-
-	fv, ok1 := v.(float64)
-	if !ok1 {
-		return 0, false
-	}
-	return fv, true
+func (r *RdsRecord) ClearFlg() {
+	r.flg = 0
 }
 
-func (r *RdsRecord) setFloat64(name string, v float64) {
-	r.data[name] = v
-	r.flg[name] = true
-}
-
-//获取设置int
-func (r *RdsRecord) getInt(name string) (int, bool) {
-	v, ok := r.data[name]
-	if !ok {
-		return 0, false
-	}
-
-	iv, ok1 := v.(int)
-	if !ok1 {
-		return 0, false
-	}
-	return iv, true
-}
-
-func (r *RdsRecord) setInt(name string, v int) {
-	r.data[name] = v
-	r.flg[name] = true
-}
-
-//获取设置int64
-func (r *RdsRecord) getInt64(name string) (int64, bool) {
-	v, ok := r.data[name]
-	if !ok {
-		return 0, false
-	}
-
-	iv, ok1 := v.(int64)
-	if !ok1 {
-		return 0, false
-	}
-	return iv, true
-}
-
-func (r *RdsRecord) setInt64(name string, v int64) {
-	r.data[name] = v
-	r.flg[name] = true
-}
-
-//获取设置strng
-func (r *RdsRecord) getString(name string) (string, bool) {
-	v, ok := r.data[name]
-	if !ok {
-		return "", false
-	}
-
-	sv, ok1 := v.(string)
-	if !ok1 {
-		return "", false
-	}
-	return sv, true
-}
-
-func (r *RdsRecord) setString(name string, v string) {
-	r.data[name] = v
-	r.flg[name] = true
-}
-
-func (r *RdsRecord) Update() {
+func (r *RdsRecord) Update(obj interface{}) {
 	upmap := make(map[string]string)
-	for k, flg := range r.flg {
-		if flg {
-			v := r.data[k]
-			switch reflect.TypeOf(v).Kind() {
-			case reflect.String:
-				upmap[k] = v.(string)
-			case reflect.Int:
-				upmap[k] = strconv.Itoa(v.(int))
-			case reflect.Int64:
-				upmap[k] = strconv.FormatInt(v.(int64), 10)
-			case reflect.Float64:
-				upmap[k] = strconv.FormatFloat(v.(float64), 'f', -1, 64)
-			case reflect.Bool:
-				upmap[k] = strconv.FormatBool(v.(bool))
-			}
-		}
-	}
-	ok, err := r.db.HMSet(r.keyname, upmap).Result()
-	_, _ = ok, err
-}
-
-func (r *RdsRecord) LoadFromRds(key string, tv reflect.Value) {
-	mapobj, err := r.db.HGetAll(r.keyname).Result()
-	if err != nil {
+	fmt.Println("obj  === ", obj, r.flg)
+	tmp := reflect.ValueOf(obj)
+	if tmp.Type().Kind() != reflect.Ptr {
+		zaplogger.Error("at record obj not ptr .. ")
 		return
 	}
-	for k, strval := range mapobj {
-		field := tv.FieldByName(k)
-		switch field.Type().Kind() {
-		case reflect.String:
-			r.data[k] = strval
-		case reflect.Int:
-			iv, err := strconv.Atoi(strval)
-			if err != nil {
-				continue
+
+	value := tmp.Elem()
+	types := value.Type()
+	index := 0
+
+	for r.flg != 0 {
+		if (r.flg & 1) != 0 {
+			v := value.Field(index)
+			switch v.Type().Kind() {
+			case reflect.String:
+				upmap[types.Field(index).Name] = v.String()
+			case reflect.Int:
+				upmap[types.Field(index).Name] = strconv.Itoa(int(v.Int()))
+			case reflect.Int64:
+				upmap[types.Field(index).Name] = strconv.FormatInt(v.Int(), 10)
+			case reflect.Float64:
+				upmap[types.Field(index).Name] = strconv.FormatFloat(v.Float(), 'f', -1, 64)
+			case reflect.Bool:
+				upmap[types.Field(index).Name] = strconv.FormatBool(v.Bool())
 			}
-			r.data[k] = iv
-		case reflect.Int64:
-			i64v, err := strconv.ParseInt(strval, 10, 64)
-			if err != nil {
-				continue
-			}
-			r.data[k] = i64v
-		case reflect.Float64:
-			fv, err := strconv.ParseFloat(strval, 64)
-			if err != nil {
-				continue
-			}
-			r.data[k] = fv
-		case reflect.Bool:
-			bv, err := strconv.ParseBool(strval)
-			if err != nil {
-				continue
-			}
-			r.data[k] = bv
 		}
+		index += 1
+		r.flg = r.flg >> 1
+	}
+
+	fmt.Println("redis ceche data is ", upmap)
+	_, err := r.db.HMSet(r.keyname, upmap).Result()
+	if err != nil {
+		zaplogger.Debug("at update roomplayer error", zap.String("err:=", err.Error()))
 	}
 }
 
-func (r *RdsRecord) LoadFromMap(obj map[string]interface{}) {
-	r.data = obj
+func (r *RdsRecord) LoadFromRds(obj interface{}) bool {
+	mapobj, err := r.db.HGetAll(r.keyname).Result()
+	fmt.Println("redis get  data is ", mapobj)
+	if err != nil {
+		return false
+	}
+
+	tmp := reflect.ValueOf(obj)
+	if tmp.Type().Kind() != reflect.Ptr {
+		zaplogger.Error("at record obj not ptr .. ")
+		return false
+	}
+
+	value := tmp.Elem()
+	types := value.Type()
+
+	numfiled := value.NumField()
+	if numfiled < 1 {
+		return false
+	}
+	fideCount := uint64(numfiled - 1) // -1 is RdsRecord
+	r.flg = (1 << fideCount) - 1
+	index := 0
+	for r.flg != 0 {
+		v := value.Field(index)
+		strval, ok := mapobj[types.Field(index).Name]
+		if !ok {
+			zaplogger.Error("at LoadFromRds field can't find field", zap.String("obj", types.Field(index).Name))
+			return false
+		}
+		if !v.CanSet() {
+			zaplogger.Error("at LoadFromRds field can'tset")
+			return false
+		}
+
+		switch v.Type().Kind() {
+		case reflect.String:
+			v.SetString(strval)
+		case reflect.Int:
+			i64v, err := strconv.ParseInt(strval, 10, 64)
+			if err == nil {
+				v.SetInt(i64v)
+			}
+
+		case reflect.Int64:
+			i64v, err := strconv.ParseInt(strval, 10, 64)
+			if err == nil {
+				v.SetInt(i64v)
+			}
+
+		case reflect.Float64:
+			fv, err := strconv.ParseFloat(strval, 64)
+			if err == nil {
+				v.SetFloat(fv)
+			}
+
+		case reflect.Bool:
+			bv, err := strconv.ParseBool(strval)
+			if err == nil {
+				v.SetBool(bv)
+			}
+		}
+		index += 1
+		r.flg = r.flg >> 1
+	}
+	return true
 }
 
 func (r *RdsRecord) Delete() {
